3765b547a27badf50b9853451ae7f90a170189c6
Pablo <pablito@me.com>
2023-04-06 10:56:00 +0100

    fixed exec_params

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -42,15 +42,15 @@ class DatabaseConnection
   #   @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
   # end
 
-  # # This method executes an SQL query 
-  # # on the database, providing some optional parameters
-  # # (you will learn a bit later about when to provide these parameters).
-  # def self.exec_params(query, params)
-  #   if @connection.nil?
-  #     raise 'DatabaseConnection.exec_params: Cannot run a SQL query as the connection to'\
-  #     'the database was never opened. Did you make sure to call first the method '\
-  #     '`DatabaseConnection.connect` in your app.rb file (or in your tests spec_helper.rb)?'
-  #   end
-  #   @connection.exec_params(query, params)
-  # end
+  # This method executes an SQL query 
+  # on the database, providing some optional parameters
+  # (you will learn a bit later about when to provide these parameters).
+  def self.exec_params(query, params)
+    if @connection.nil?
+      raise 'DatabaseConnection.exec_params: Cannot run a SQL query as the connection to'\
+      'the database was never opened. Did you make sure to call first the method '\
+      '`DatabaseConnection.connect` in your app.rb file (or in your tests spec_helper.rb)?'
+    end
+    @connection.exec_params(query, params)
+  end
 end
